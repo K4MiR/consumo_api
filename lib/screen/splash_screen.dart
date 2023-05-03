2d1d405e38b03bf4b9cdc8762a9e444ade0ff916
K4MiR <cleberrobson16@gmail.com>
2023-05-02 23:00:08 -0300

    ajuste de code clean

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,11 +1,6 @@
 import 'package:consumo_api/screen/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:lottie/lottie.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-
 
 class Splash_Screen extends StatefulWidget {
   const Splash_Screen({Key? key}) : super(key: key);
@@ -18,33 +13,23 @@ class _Splash_ScreenState extends State<Splash_Screen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(
-        Duration(
-            seconds: 3
-        ),
-            (){
-          Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => Home_Screen(),
-              )
-          );
-        }
-    );
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Home_Screen(),
+          ));
+    });
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        width: MediaQuery
-            .of(context)
-            .size
-            .width,
+        width: MediaQuery.of(context).size.width,
         color: Colors.white,
-        child:
-        Center(child: Lottie.asset('assets/lotties/location1.json', width: 250)),
+        child: Center(
+            child: Lottie.asset('assets/lotties/location1.json', width: 250)),
       ),
     );
   }
 }
-
